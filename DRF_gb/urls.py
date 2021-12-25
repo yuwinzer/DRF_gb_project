1f0a82f98dc09fc39b75412fc36510e3686e3618
yuwinzer <yu.moskalev@gmail.com>
2021-12-25 20:18:55 +0200

    fixed the render of users

--- DRF_gb/urls.py
+++ DRF_gb/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from usersapp.views import UserModelViewSet
+from usersapp.views import UserReadUpdateViewSet
 from todoapp.views import ProjectViewSet, ToDoNoDelViewSet
 
 router = DefaultRouter()
-router.register('users', UserModelViewSet)
+router.register('users', UserReadUpdateViewSet)
 router.register('projects', ProjectViewSet)
 router.register('todos', ToDoNoDelViewSet)
 
--- usersapp/models.py
+++ usersapp/models.py
@@ -1,8 +1,12 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
-   first_name = models.CharField(max_length=64)
-   last_name = models.CharField(max_length=64)
-   username = models.CharField(max_length=64, unique=True)
-   email = models.EmailField(max_length=64, unique=True)
+    username = models.CharField(max_length=64, unique=True)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.EmailField(max_length=64, unique=True)
+
+    def __str__(self):
+        return self.username
